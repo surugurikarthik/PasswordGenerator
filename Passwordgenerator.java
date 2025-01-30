dd4a600db353b2fb36048c24f65be55b7c0607a6
surugurikarthik <surugoorikarthik@gmail.com>
2025-01-30 09:39:59 +0530

    Add files via upload

---
+++ Passwordgenerator.java
@@ -0,0 +1,13 @@
+import java.util.Random;
+public class Passwordgenerator {
+
+    public static void main(String[] args) {
+Random k= new Random();
+int m=k.nextInt(120);
+System.out.print(m);
+String b="@123ASdasd";
+System.out.print(b);
+
+    }
+    
+}
